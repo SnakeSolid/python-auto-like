f8096a244c04db58d065b04f7fcf8d15884f49b0
Shabanov Anton <iamshabanov@yandex.ru>
2024-03-30 15:03:42 +0300

    show like/dislike proportion

--- utilities/print-database.py
+++ utilities/print-database.py
@@ -21,10 +21,24 @@ def start(database_path: str,
         [0])
     print()
 
+    n_like = 0
+    n_dislike = 0
+
     for (mark, count) in connection.execute(
             "select mark, count(*) from photo group by mark order by mark"
     ).fetchall():
+        if mark == "like":
+            n_like = count
+        elif mark == "dislike":
+            n_dislike = count
+
         print("N", mark, ":", count)
+
+    n_total = n_like + n_dislike
+
+    if n_total > 0:
+        print("  {:0.1f}% / {:0.1f}% (like / dislike)".format(
+            100 * n_like / n_total, 100 * n_dislike / n_total))
 
     if not statistics:
         print()
